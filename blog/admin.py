e913d1cec1a024d62549e6342375763de63d95a2
Roman <98953084+Rokuflam@users.noreply.github.com>
2023-08-17 16:16:12 +0300

    Finish Section 9
    Changes to be committed:
    	modified:   blog/__pycache__/admin.cpython-311.pyc
    	modified:   blog/__pycache__/models.cpython-311.pyc
    	modified:   blog/__pycache__/views.cpython-311.pyc
    	modified:   blog/admin.py
    	new file:   blog/migrations/0001_initial.py
    	new file:   blog/migrations/__pycache__/0001_initial.cpython-311.pyc
    	modified:   blog/models.py
    	modified:   blog/static/blog/post-detail.css
    	modified:   blog/templates/blog/includes/post.html
    	modified:   blog/templates/blog/post-detail.html
    	modified:   blog/views.py
    	modified:   db.sqlite3

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
+
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'date', 'tags')
+    list_display = ('title', 'date', 'author')
+    
+    prepopulated_fields = {'slug': ('title', )}
