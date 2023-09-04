ee98010aff81c46faef2d83b58bef63461cd7abe
Roman <98953084+Rokuflam@users.noreply.github.com>
2023-09-04 13:16:40 +0300

    Added to possibility to write a comment
    Changes to be committed:
    	new file:   blog/__pycache__/forms.cpython-311.pyc
    	modified:   blog/__pycache__/models.cpython-311.pyc
    	modified:   blog/__pycache__/urls.cpython-311.pyc
    	modified:   blog/__pycache__/views.cpython-311.pyc
    	new file:   blog/forms.py
    	new file:   blog/migrations/0003_comment.py
    	new file:   blog/migrations/__pycache__/0003_comment.cpython-311.pyc
    	modified:   blog/models.py
    	modified:   blog/static/blog/post-detail.css
    	modified:   blog/templates/blog/index.html
    	modified:   blog/templates/blog/post-detail.html
    	modified:   blog/urls.py
    	modified:   blog/views.py
    	modified:   db.sqlite3

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,55 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
-all_posts = []
+from .forms import CommentForm
 
-def starting_page(request):
-    latest_sorted_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, 'blog/index.html', {
-        'all_posts': latest_sorted_posts
-    })
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        return queryset[:3]
 
-def posts(request):
-    latest_sorted_posts = Post.objects.all().order_by('-date')
-    return render(request, "blog/all-posts.html", {
-        'all_posts': latest_sorted_posts
-    })
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        'post': identified_post,
-        'post_tegs': identified_post.tags.all() 
-        })
+class AllPostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
+    
+
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, 'blog/post-detail.html', context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.Post)
+        post = Post.objects.get(slug=slug)
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post-detail-page'), atgs=[slug])
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, 'blog/post-detail.html', context)
